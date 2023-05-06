d5195065511156569c6c642d010798ca3450080b
tum-jackie <jacklinetum17@gmail.com>
2023-05-06 23:22:39 +0300

    new file

--- celcius.py
+++
@@ -1,7 +0,0 @@
-def farenheirt():
-    """ returns the temprature in farenheit"""
-    celcius = float(input( "enter the temprature in celcius: "))
-    f = celcius * 9/5 + 32
-    return f
-
-print(f"temperature in farenheit is  {farenheirt()}")
--- kanye-api/credentials.py
+++
@@ -1,4 +0,0 @@
-weather_key = "2ce180b93bd1480942b1cc598a7933b9"
-
-auth_token= "e35964d4dc2c7a4b046eaa359b75da93" # Your Twilio API Key
-account_sid = "ACbde4d801388d4a97d14753fb92a5f7ac"
