2304b15ccc3bda048d19b465e082a110f9bd6d67
tum-jackie <jacklinetum17@gmail.com>
2023-05-08 06:31:59 +0300

    new assignment

--- assignment 3/even_num.py
+++ assignment 3/even_num.py
@@ -1,9 +1,8 @@
-NUM = [1,2,3,4,5,6]
-
 def even_num(NUM):
     """Prints even numbers in a list"""
     for i in NUM:
         if i % 2 == 0:
             print(i)
 
+NUM = [1,2,3,4,5,6]
 even_num(NUM)
